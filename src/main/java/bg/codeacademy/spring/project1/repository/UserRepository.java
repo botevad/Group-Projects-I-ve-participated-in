f33dc6218fcde2aca0def789961b1730867cf3bd
rado <rado@localhost>
2020-04-06 15:57:33 +0300

    Small fixes on Comment Service (filter by date); Added method to search users by username; Other fixes

--- src/main/java/bg/codeacademy/spring/project1/repository/UserRepository.java
+++ src/main/java/bg/codeacademy/spring/project1/repository/UserRepository.java
@@ -2,11 +2,11 @@ package bg.codeacademy.spring.project1.repository;
 
 import bg.codeacademy.spring.project1.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends JpaRepository<User, Integer>
 {
+    User findUserByUsername(String username);
 }
