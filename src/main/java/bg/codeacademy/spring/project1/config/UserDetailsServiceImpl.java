5777a2f5bf6bd297be9e492bae69ffdfd57eb052
georgi-pehlivanov <georgi-pehlivanov@localhost>
2020-04-08 20:53:50 +0300

    Fix UserDetailsServiceImpl access users through UserService

--- src/main/java/bg/codeacademy/spring/project1/config/UserDetailsServiceImpl.java
+++ src/main/java/bg/codeacademy/spring/project1/config/UserDetailsServiceImpl.java
@@ -8,9 +8,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService
 {
 
@@ -27,22 +28,17 @@ public class UserDetailsServiceImpl implements UserDetailsService
     List<User> users = userService.getUsers();
     if (users.isEmpty()) {
       // first start, create default admin user
-      User admin = new User(true);
-      admin.setUsername("admin");
-      admin.setRole(Role.ADMIN);
-      admin.setPassword(new BCryptPasswordEncoder().encode("123456"));
-      userService.createUser(admin.getUsername(),admin.getPassword(),admin.getRole());
-      users.add(admin);
+      userService.createUser("admin", "123456", Role.ADMIN);
     }
-    for (bg.codeacademy.spring.project1.model.User user : users) {
-      if (user.getUsername().equals(userName)) {
-        org.springframework.security.core.userdetails.User.UserBuilder builder = null;
-        builder = org.springframework.security.core.userdetails.User.withUsername(userName);
-        builder.password(user.getPassword());
-        builder.roles(user.getRole().toString());
-        return builder.build();
-      }
+
+    User user = userService.getUser(userName);
+    if (null == user) {
+      throw new UsernameNotFoundException("User not found.");
     }
-    throw new UsernameNotFoundException("User not found.");
+    return org.springframework.security.core.userdetails.User.withUsername(userName)
+        .password(user.getPassword())
+        .roles(user.getRole().toString())
+        .build();
+
   }
 }
